9b30e4285348a592535c6853a1787412bf6a1b6d
christoferNK22 <christofernathanael116@gmail.com>
2025-06-21 02:44:09 +0700

    add: transaction models

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Transaction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total',
+        'payment_method',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(TransactionItem::class);
+    }
 }
